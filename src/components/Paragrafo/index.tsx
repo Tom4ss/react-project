94255f499f1e82dc766567e6e4a815f24694f4c6
54M07 <tomas_ss@aol.com>
2024-08-12 10:55:33 -0300

    estilizando a sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -2,10 +2,13 @@ import { P } from "./styles"
 
 export type Props = {
   children: string
-  tipo?: string
+  tipo?: 'principal' | 'secundario'
+  fontSize?: number
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => <P>{children}</P>
+const Paragrafo = ({ children, tipo = 'principal', fontSize }: Props) => (
+  <P fontSize={fontSize} tipo={tipo}>{children}</P>
+)
 
 
 export default Paragrafo
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/54M07.png" />
+    <SidebarContainer>
+    <Avatar />
     <Titulo fontSize={20}>Tomás Santana</Titulo>
+    <Paragrafo tipo="secundario" font-size={16}>54M07</Paragrafo>
+    <Descricao tipo="principal" fontSize={12}>Engenheiro Front-End</Descricao>
+    <BotaoTema>Trocar Tema</BotaoTema>
+
+    </SidebarContainer>
   </aside>
 )
 
